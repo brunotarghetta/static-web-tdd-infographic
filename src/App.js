27e8ce21a8e785a0606fb4715eb366613d2e05ad
Bruno Targhetta <bruno_targhetta@hotmail.com>
2021-06-01 08:56:09 -0300

    Agregado de bootrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/css/starter.css';
+import './assets/js/starter.js'
+
 
 function App() {
   return (
     <div className="App">
+      <nav class="navbar navbar-light bg-light">
+          <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+          </div>
+        </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
